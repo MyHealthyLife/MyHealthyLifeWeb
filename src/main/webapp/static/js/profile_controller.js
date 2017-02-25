5fefa71f73a7ae5862ce6bb2d6f7830db5ec74fc
StefanoTavonatti93 <stefano.tavonatti@studenti.unitn.it>
2017-02-25 16:16:02 +0100

    cors filter

--- src/main/webapp/static/js/profile_controller.js
+++ src/main/webapp/static/js/profile_controller.js
@@ -1,5 +1,28 @@
 var app=angular.module('profile', []);
 
-app.controller('profileController', function ($scope,$rootScope, $http) {
+var cemtric01_basic="https://centric01-main.herokuapp.com";
+
+app.controller('user_data', function ($scope,$rootScope, $http) {
 	
+	$scope.loadUserData = function () {
+        console.log('loadFeeds called.');
+       
+        //$scope.loadData();
+        $http({
+            url: cemtric01_basic+"/user/data/morpheuss93",
+            method: 'GET',
+            params: {
+            }
+        }).success(function (data) {
+            $scope.user_data = data;
+        });
+        console.log($scope.user_data);
+       
+    };
+    
+    $scope.loadUserData();
+	
+    $scope.loadData=function(){
+        
+    };
 });
---
+++ src/myhealthylife/myhealthylifeweb/filters/CorsFilter.java
@@ -0,0 +1,58 @@
+package myhealthylife.myhealthylifeweb.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class CorsFilter
+ */
+public class CorsFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public CorsFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("CorsFilter...");
+		if(response instanceof HttpServletResponse){
+	        HttpServletResponse alteredResponse = ((HttpServletResponse)response);
+	        addCorsHeader(alteredResponse);
+	    }
+		chain.doFilter(request, response);
+	}
+	
+	private void addCorsHeader(HttpServletResponse response){
+        //TODO: externalize the Allow-Origin
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+        response.addHeader("Access-Control-Max-Age", "1728000");
+    }
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
