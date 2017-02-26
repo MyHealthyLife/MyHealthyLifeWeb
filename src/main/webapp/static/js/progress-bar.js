7f695fc45c9ad12016256d10ec41b62ab2474138
SimoneScalco93 <simone.scalco@studenti.unitn.it>
2017-02-26 15:03:09 +0100

    Changed link to JSP page on login

--- src/main/webapp/static/js/progress-bar.js
+++ src/main/webapp/static/js/progress-bar.js
@@ -1,19 +1,1 @@
- $(document).ready(
-		 updateProgressBars=function() {
-      $('.progress .progress-bar').css("width",
-                function() {
-                    return $(this).attr("aria-valuenow") + "%";
-                }
-        )
-    });
-
  
- 
- 
-updateProgressBars = (function() {
-     $('.progress .progress-bar').css("width",
-               function() {
-                   return $(this).attr("aria-valuenow") + "%";
-               }
-       )
-   });
