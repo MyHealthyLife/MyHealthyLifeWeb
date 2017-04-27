1a20998b025c7a7b94b4bfa2f7e9bdd9ef094cd5
SimoneScalco93 <simone.scalco@studenti.unitn.it>
2017-04-27 15:38:04 +0200

    Social JS functions commented

--- src/main/webapp/static/js/social.js
+++ src/main/webapp/static/js/social.js
@@ -1,29 +1,34 @@
 
+// Data for the autocomplete modules
 var availableTags = ["No option available"];
 var availableUsernames = ["No username present"];
 var counterFoodsInRecipe = 2;
 
+// Loads the data for the autocomplete input fields
 function autoCompleteModules(){
 	
+	// Gets the measures from the angular controller scope
 	var measures = $('[ng-controller="addFoodController"]').scope().foodTypes;
 	
 	if(measures.length!=0) {
 		availableTags = [];
 	}
 	
+	// Pushes the measure types in the variable for the autocomplete
 	for(i=0;i<measures.length;i++) {
 		
 		availableTags.push(measures[i].category);
 		
 	}
 	
-
+	// Gets the list of usernames from the angular controller scope
 	var usernamesFromController = $('[ng-controller="dedicateSentenceController"]').scope().usernames;
 
 	if(usernamesFromController.length!=0) {
 		availableUsernames = [];
 	}
 	
+	// Pushes the list of usernames in the variable for the autocomplete
 	for(i=0;i<usernamesFromController.length;i++) {
 		
 		availableUsernames.push(usernamesFromController[i]);
@@ -35,8 +40,13 @@ function autoCompleteModules(){
 	
 }
 
+// When the document has been fully loaded
 $(document).ready(function() {
+	
+	// Function that repeats every 3 seconds
 	setInterval(function(){
+		
+		// Calls the function to load the data for the autocomplete modules
 	    autoCompleteModules();
 	  
 	$("#foodTypeAuto").autocomplete({
@@ -49,9 +59,11 @@ $(document).ready(function() {
 
 	    $("#hiddenCloneRecipe").hide();
 
+	// Adds a foods when clicking on "Add food" button in recipes section
 	$("#num").click(function () {
 	  $(".Box:first").clone().appendTo("#form");
 	  
+	  // Clones the div and changes some attributes
 		$("#boxToClone").attr("class", String(counterFoodsInRecipe));
 		$("#boxToClone").attr("id", "view");
 
@@ -63,7 +75,7 @@ $(document).ready(function() {
 	});
 	
 
-
+	// Removes a food when clicking on "Remove food" button in recipes section
 	$("#rmv").click(function () {
 		 counterFoodsInRecipe--;
 		 $('.' + String(counterFoodsInRecipe)).remove();
